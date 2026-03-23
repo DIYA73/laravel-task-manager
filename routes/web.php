07d165751d281d5fe3c9725ad2703dfd41be018a
didi <diyaismahil7@gmail.com>
2026-03-23 22:17:46 +0300

    fix: add root redirect to login

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,17 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+// Root redirect to login
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::get('/dashboard', function () {
     $user = auth()->user();
-
-    if (!$user) {
-        return redirect('/login');
-    }
-
     $tasks = $user->tasks;
     return view('dashboard', compact('tasks'));
 })->middleware(['auth'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
 
     // TASKS
@@ -21,7 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/tasks/{task}/doing', [TaskController::class, 'doing']);
     Route::post('/tasks/{task}/done', [TaskController::class, 'done']);
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
-Route::post('/tasks/{task}/move', [TaskController::class, 'move']);
+    Route::post('/tasks/{task}/move', [TaskController::class, 'move']);
+
     // PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
