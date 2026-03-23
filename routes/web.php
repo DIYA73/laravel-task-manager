56fa59e2472810314f969e79028f9f5f8f51409b
didi <diyaismahil7@gmail.com>
2026-03-23 18:11:13 +0300

    test route

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return "🔥 Laravel is working!";
 });
-
 Route::get('/dashboard', function () {
     $tasks = auth()->user()->tasks;
     return view('dashboard', compact('tasks'));
